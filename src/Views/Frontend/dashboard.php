29aac65e2eee67608fd63a9ed4a652439cefac81
ilyesb36 <ilyesb36130@gmail.com>
2021-12-09 15:31:44 +0100

    ajout taff inutile stephane

---
+++ src/Views/Frontend/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+
+<h1> Ajouter un post </h1>
+
+<div>
+    <form method="post" action="">
+        <p>
+        	<label value = "Title"> Intitulé du post: </label><input type = "text" name="postName"></input>
+        </p>
+        <p>
+        	<label value ="Content"> Contenu </label></br>
+        	<textarea name = "postContent" rows= "10" cols= "60"></textarea>
+        </p>
+        <p>
+        	<input type= "submit" value= "Poster" ></input>
+        </p>
+    </form>
+</div>
